13c5d1be4047de66312e34d6bdde694166f998f2
Micha <michaelanolte18@gmx.de>
2020-04-21 10:17:53 +0200

    Session2

---
+++ session2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace session2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
